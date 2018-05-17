3626d80429998fc93ce2ab2342848393c3bf9390
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2018-05-17 07:38:32 -0500

    feat: add taxis cucuta pyp info

--- src/lib/__tests__/cucuta.js
+++ src/lib/__tests__/cucuta.js
@@ -8,46 +8,54 @@ describe("Test cases for Cúcuta", () => {
       date: "2018-01-01", // Año Nuevo
       pypCases: {
         particulares: "NA",
-        motos: "NA"
+        motos: "NA",
+        taxis: "NA"
       }
     },
     {
       date: "2018-03-19", // lunes festivo: San José
       pypCases: {
         particulares: "NA",
-        motos: "NA"
+        motos: "NA",
+        taxis: "NA"
       }
     },
     {
       date: "2018-04-07", // sábado 7 de abril de 2018
       pypCases: {
         particulares: "NA",
-        motos: "NA"
+        motos: "NA",
+        taxis: "NA"
       }
     },
     {
       date: "2018-02-04", // domingo 4 de febrero de 2018
       pypCases: {
         particulares: "NA",
-        motos: "NA"
+        motos: "NA",
+        taxis: "NA"
       }
     },
     {
-      date: "2018-04-30", // lunes 9 de abril de 2018
-      pypCases: {}
+      date: "2018-04-30", // lunes 30 de abril de 2018
+      pypCases: {
+        taxis: "0"
+      }
     },
     {
       date: "2018-04-09", // lunes 9 de abril de 2018
       pypCases: {
         particulares: "0-9",
-        motos: "0-9"
+        motos: "0-9",
+        taxis: "1"
       }
     },
     {
       date: "2018-05-16", // miércoles 16 de mayo de 2018
       pypCases: {
         particulares: "2-7",
-        motos: "2-7"
+        motos: "2-7",
+        taxis: "8"
       }
     }
   ];
--- src/lib/cucuta.js
+++ src/lib/cucuta.js
@@ -90,6 +90,53 @@ module.exports = {
           return pyp[pypFuncs.getDay(date)];
         });
       }
+    },
+    taxis: {
+      info: {
+        vehicleClasses: [
+          "Vehículos de transporte público individual de pasajeros - Taxis"
+        ],
+        decrees: [
+          {
+            name: "Decretos 0239 del 4 de abril del 2017",
+            url:
+              "http://www.areacucuta.com/wp-content/uploads/2017/04/Decreto-Pico-y-Placa-Placa-y-D%C3%ADa.pdf"
+          },
+          {
+            name: "Decretos 0330 del 22 de mayo del 2017",
+            url:
+              "http://cucutaonline.com/wp-content/uploads/2017/05/Decreto-0330-pico-placa-modificado.pdf"
+          }
+        ],
+        days: ["Lunes a viernes hábiles"],
+        hours: ["7:30 - 23:00"],
+        scheme:
+          "Medida de tránsito de RESTRICCIÓN VEHICULAR denominada PLACA – DÍA en la ciudad de San José de Cúcuta para VEHÍCULOS DE SERVICIO PÚBLICO TIPO TAXI, VEHÍCULOS DE PLACA EXTRANJERA (automóviles, motocicletas, camionetas, camiones, volquetas) y VEHÍCULOS DE PLACA NACIONAL (automóviles, motocicletas, camionetas, camiones, volquetes)."
+      },
+      name: "Taxis",
+      na: [0, 6],
+      pyp(date) {
+        return pypFuncs.pyp(date, this.na, true, () => {
+          const pyp = [
+            {
+              1: "0", // lunes
+              2: "9", // martes
+              3: "8", // miércoles
+              4: "7", // jueves
+              5: "6" // viernes
+            },
+            {
+              1: "1", // lunes
+              2: "2", // martes
+              3: "3", // miércoles
+              4: "4", // jueves
+              5: "5" // viernes
+            }
+          ];
+          const week = pypFuncs.getWeek(date) % 2;
+          return pyp[week][pypFuncs.getDay(date)];
+        });
+      }
     }
   }
 };
