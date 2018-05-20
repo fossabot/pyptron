377a371833d379b6231780a21b6dd7fd3058edda
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2018-05-20 14:20:07 -0500

    fix: update wrong motos name

--- src/lib/popayan.js
+++ src/lib/popayan.js
@@ -56,7 +56,7 @@ module.exports = {
         hours: ["7:00 - 19:00"],
         scheme: "Último dígito del número de la placa según el esquema"
       },
-      name: "Particulares",
+      name: "motos",
       na: [0, 6],
       pyp(date) {
         return pypFuncs.pyp(date, this.na, true, () => {
