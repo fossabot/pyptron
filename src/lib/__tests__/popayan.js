ff5156000dbb3bca9a3ddddd7ca3c50026c4b637
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2018-05-24 21:12:38 -0500

    fix: use correct numbers and add tests

--- src/lib/__tests__/popayan.js
+++ src/lib/__tests__/popayan.js
@@ -4,38 +4,38 @@ const times = ["T00:00:00.0000-05:00", "T23:59:59.999-05:00"];
 
 describe(`Test cases for  ${popayan.name}`, () => {
   const dateCases = [
-    // {
-    //   date: "2018-01-01", // Año Nuevo
-    //   pypCases: {
-    //     particulares: "NA",
-    //     motos: "NA",
-    //     tpc: "NA"
-    //   }
-    // },
-    // {
-    //   date: "2018-03-19", // lunes festivo: San José
-    //   pypCases: {
-    //     particulares: "NA",
-    //     motos: "NA",
-    //     tpc: "NA"
-    //   }
-    // },
-    // {
-    //   date: "2018-04-07", // sábado 7 de abril de 2018
-    //   pypCases: {
-    //     particulares: "NA",
-    //     motos: "NA",
-    //     tpc: "NA"
-    //   }
-    // },
-    // {
-    //   date: "2018-02-04", // domingo 4 de febrero de 2018
-    //   pypCases: {
-    //     particulares: "NA",
-    //     motos: "NA",
-    //     tpc: "NA"
-    //   }
-    // },
+    {
+      date: "2018-01-01", // Año Nuevo
+      pypCases: {
+        particulares: "NA",
+        motos: "NA",
+        tpc: "NA"
+      }
+    },
+    {
+      date: "2018-03-19", // lunes festivo: San José
+      pypCases: {
+        particulares: "NA",
+        motos: "NA",
+        tpc: "NA"
+      }
+    },
+    {
+      date: "2018-04-07", // sábado 7 de abril de 2018
+      pypCases: {
+        particulares: "NA",
+        motos: "NA",
+        tpc: "NA"
+      }
+    },
+    {
+      date: "2018-02-04", // domingo 4 de febrero de 2018
+      pypCases: {
+        particulares: "NA",
+        motos: "NA",
+        tpc: "NA"
+      }
+    },
     {
       date: "2018-02-02", // lunes 9 de abril de 2018
       pypCases: {
@@ -75,17 +75,33 @@ describe(`Test cases for  ${popayan.name}`, () => {
     {
       date: "2018-04-09", // lunes 9 de abril de 2018
       pypCases: {
-        particulares: "5-6",
-        motos: "5-6",
+        particulares: "3-4",
+        motos: "3-4",
         tpc: "3"
       }
     },
     {
       date: "2018-05-16", // miércoles 16 de mayo de 2018
       pypCases: {
-        particulares: "9-0",
-        motos: "9-0"
-        // tpc: "6"
+        particulares: "7-8",
+        motos: "7-8",
+        tpc: "6"
+      }
+    },
+    {
+      date: "2018-07-03", // martes 3 de Julio de 2018
+      pypCases: {
+        particulares: "3-4",
+        motos: "3-4",
+        tpc: "7"
+      }
+    },
+    {
+      date: "2018-10-18", // jueves 18 de octubre de 2018
+      pypCases: {
+        particulares: "7-8",
+        motos: "7-8",
+        tpc: "0"
       }
     }
   ];
