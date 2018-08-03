bb7baf0be6bb66a53993d466f48509eb2e96f444
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2018-08-03 09:00:21 -0500

    chore(medellin): add more test cases

--- src/lib/__tests__/medellin.js
+++ src/lib/__tests__/medellin.js
@@ -53,11 +53,59 @@ describe("Specific cases", () => {
       }
     },
     {
-      date: "2018-08-06", // miércoles 16 de mayo de 2018
+      date: "2018-08-03", // viernes 3 de agosto de 2018
+      pypCases: {
+        particulares: "0-1-2-3",
+        motos: "6-7",
+        taxis: "8"
+      }
+    },
+    {
+      date: "2018-08-06", // lunes 6 de agosto de 2018
       pypCases: {
         particulares: "0-1-2-3",
         motos: "6-7",
         taxis: "3"
+      }
+    },
+    {
+      date: "2018-09-12", // miércoles 12 de septiembre de 2018
+      pypCases: {
+        particulares: "8-9-0-1",
+        motos: "0-1",
+        taxis: "4"
+      }
+    },
+    {
+      date: "2018-10-30", // martes 30 de octubre de 2018
+      pypCases: {
+        particulares: "4-5-6-7",
+        motos: "8-9",
+        taxis: "7"
+      }
+    },
+    {
+      date: "2018-11-08", // jueves 8 de noviembre de 2018
+      pypCases: {
+        particulares: "2-3-4-5",
+        motos: "2-3",
+        taxis: "8"
+      }
+    },
+    {
+      date: "2018-12-21", // viernes 21 de diembre de 2018
+      pypCases: {
+        particulares: "6-7-8-9",
+        motos: "4-5",
+        taxis: "8"
+      }
+    },
+    {
+      date: "2019-01-31", // jueves 31 de enero de 2019
+      pypCases: {
+        particulares: "2-3-4-5",
+        motos: "2-3",
+        taxis: "6"
       }
     },
     {
