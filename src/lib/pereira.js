b0a949942c132ac414b9342f976091f8ab014173
Mauricio Robayo <archemiro@gmail.com>
2018-08-18 12:15:32 -0500

    feat(pereira): add decrees from cdn and observations

--- src/lib/pereira.js
+++ src/lib/pereira.js
@@ -8,18 +8,19 @@ export default {
         vehicleClasses: ["Vehículos automotores de servicio particular"],
         decrees: [
           {
-            name: "Decreto 193 de marzo de 2015",
-            url: "http://amco.gov.co/Archivos/Articulos/Documentos/00001082.pdf"
+            name: "Decreto 193 del 6 de marzo de 2015",
+            url:
+              "https://cdn.pyphoy.com/pereira/decreto-193-del-6-de-marzo-de-2015.pdf"
           },
           {
             name: "Decreto 516 del 21 de julio de 2015",
             url:
-              "http://www.transitopereira.gov.co/app/webroot/index.php/intradocuments/webDownload/decreto_516_del_21_julio_2015_393"
+              "https://cdn.pyphoy.com/pereira/decreto-516-del-21-de-julio-de-2015.pdf"
           },
           {
-            name: "Decreto 241 del del 2015",
+            name: "Decreto 241 del 26 de marzo de 2016",
             url:
-              "http://www.transitopereira.gov.co/app/webroot/index.php/intradocuments/webDownload/decreto_241_del_2015_394"
+              "https://cdn.pyphoy.com/pereira/decreto-241-del-26-de-marzo-de-2016.pdf"
           }
         ],
         days: [
@@ -27,7 +28,15 @@ export default {
         ],
         hours: ["7:00 - 19:00"],
         scheme: "Último dígito del número de la placa según el esquema",
-        observations: ""
+        observations: `Se establece como restricción del pico y placa, para los vehículos particulares y motos, el área al interio del perímeto delimitado por las siguientes vías e interseccones:
+        <ul>
+          <li>Partiendo del puente Mosquera se continúa por la avenida del Rio hasta la intersección de Turín. Este corredor en ambos sentidos no estará sometido a la medida de pico y placa.</li>
+          <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la intersección de la Villa Olímpica. Este corredor en ambos sentidos estará somentido a la medida de pico y placa.</li>
+          <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la calle 50, se continúa por la calle 50 hasta la glorieta de la avenida de Las Américas. Este corredor en ambos sentidos estará sometido a la medida de pico y placa.</li>
+          <li>De la intersección de la glorieta de la calle 50, se continúa por la avenida de las Américas hasta la intersección de la calle 17 con el Terminal de Transporte. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
+          <li>De la intersección de la calle 17 con Terminal de Transporte hacía el norte hasta la intersección de la calle 17 con carrera 13. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
+          <li>De la intersección de la calle 17 con carrera 13 se continúa por la avenida del Ferrocarril hasta el viaducto y la carrera 6a con dirección a la avenida del Rio - puente Mosquera. Este corredor se excluye de la medida de pico y placa.</li>
+        </ul>`
       },
       name: "Particulares",
       na: [0, 6],
@@ -49,18 +58,19 @@ export default {
         vehicleClasses: ["Motocicletas"],
         decrees: [
           {
-            name: "Decreto 193 de marzo de 2015",
-            url: "http://amco.gov.co/Archivos/Articulos/Documentos/00001082.pdf"
+            name: "Decreto 193 del 6 de marzo de 2015",
+            url:
+              "https://cdn.pyphoy.com/pereira/decreto-193-del-6-de-marzo-de-2015.pdf"
           },
           {
             name: "Decreto 516 del 21 de julio de 2015",
             url:
-              "http://www.transitopereira.gov.co/app/webroot/index.php/intradocuments/webDownload/decreto_516_del_21_julio_2015_393"
+              "https://cdn.pyphoy.com/pereira/decreto-516-del-21-de-julio-de-2015.pdf"
           },
           {
-            name: "Decreto 241 del del 2015",
+            name: "Decreto 241 del 26 de marzo de 2016",
             url:
-              "http://www.transitopereira.gov.co/app/webroot/index.php/intradocuments/webDownload/decreto_241_del_2015_394"
+              "https://cdn.pyphoy.com/pereira/decreto-241-del-26-de-marzo-de-2016.pdf"
           }
         ],
         days: [
@@ -68,7 +78,15 @@ export default {
         ],
         hours: ["7:00 - 19:00"],
         scheme: "Primer dígito del número de la placa según el esquema",
-        observations: ""
+        observations: `Se establece como restricción del pico y placa, para los vehículos particulares y motos, el área al interio del perímeto delimitado por las siguientes vías e interseccones:
+        <ul>
+          <li>Partiendo del puente Mosquera se continúa por la avenida del Rio hasta la intersección de Turín. Este corredor en ambos sentidos no estará sometido a la medida de pico y placa.</li>
+          <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la intersección de la Villa Olímpica. Este corredor en ambos sentidos estará somentido a la medida de pico y placa.</li>
+          <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la calle 50, se continúa por la calle 50 hasta la glorieta de la avenida de Las Américas. Este corredor en ambos sentidos estará sometido a la medida de pico y placa.</li>
+          <li>De la intersección de la glorieta de la calle 50, se continúa por la avenida de las Américas hasta la intersección de la calle 17 con el Terminal de Transporte. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
+          <li>De la intersección de la calle 17 con Terminal de Transporte hacía el norte hasta la intersección de la calle 17 con carrera 13. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
+          <li>De la intersección de la calle 17 con carrera 13 se continúa por la avenida del Ferrocarril hasta el viaducto y la carrera 6a con dirección a la avenida del Rio - puente Mosquera. Este corredor se excluye de la medida de pico y placa.</li>
+        </ul>`
       },
       name: "Motos",
       na: [0, 6],
