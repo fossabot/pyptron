dac86dd7692ad37ebca0d00fc69f93711a141e65
Mauricio Robayo <archemiro@gmail.com>
2018-08-18 17:16:26 -0500

    fix(pereira): typos on observation

--- src/lib/pereira.js
+++ src/lib/pereira.js
@@ -28,14 +28,14 @@ export default {
         ],
         hours: ["7:00 - 19:00"],
         scheme: "Último dígito del número de la placa según el esquema",
-        observations: `Se establece como restricción del pico y placa, para los vehículos particulares y motos, el área al interio del perímeto delimitado por las siguientes vías e interseccones:
+        observations: `Se establece como restricción del pico y placa, para los vehículos particulares y motos, el área al interior del perímeto delimitado por las siguientes vías e interseccones:
         <ul>
-          <li>Partiendo del puente Mosquera se continúa por la avenida del Rio hasta la intersección de Turín. Este corredor en ambos sentidos no estará sometido a la medida de pico y placa.</li>
+          <li>Partiendo del puente Mosquera se continúa por la avenida del Río hasta la intersección de Turín. Este corredor en ambos sentidos no estará sometido a la medida de pico y placa.</li>
           <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la intersección de la Villa Olímpica. Este corredor en ambos sentidos estará somentido a la medida de pico y placa.</li>
           <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la calle 50, se continúa por la calle 50 hasta la glorieta de la avenida de Las Américas. Este corredor en ambos sentidos estará sometido a la medida de pico y placa.</li>
           <li>De la intersección de la glorieta de la calle 50, se continúa por la avenida de las Américas hasta la intersección de la calle 17 con el Terminal de Transporte. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
           <li>De la intersección de la calle 17 con Terminal de Transporte hacía el norte hasta la intersección de la calle 17 con carrera 13. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
-          <li>De la intersección de la calle 17 con carrera 13 se continúa por la avenida del Ferrocarril hasta el viaducto y la carrera 6a con dirección a la avenida del Rio - puente Mosquera. Este corredor se excluye de la medida de pico y placa.</li>
+          <li>De la intersección de la calle 17 con carrera 13 se continúa por la avenida del Ferrocarril hasta el viaducto y la carrera 6a con dirección a la avenida del Río - puente Mosquera. Este corredor se excluye de la medida de pico y placa.</li>
         </ul>`
       },
       name: "Particulares",
@@ -78,14 +78,14 @@ export default {
         ],
         hours: ["7:00 - 19:00"],
         scheme: "Primer dígito del número de la placa según el esquema",
-        observations: `Se establece como restricción del pico y placa, para los vehículos particulares y motos, el área al interio del perímeto delimitado por las siguientes vías e interseccones:
+        observations: `Se establece como restricción del pico y placa, para los vehículos particulares y motos, el área al interior del perímeto delimitado por las siguientes vías e interseccones:
         <ul>
-          <li>Partiendo del puente Mosquera se continúa por la avenida del Rio hasta la intersección de Turín. Este corredor en ambos sentidos no estará sometido a la medida de pico y placa.</li>
+          <li>Partiendo del puente Mosquera se continúa por la avenida del Río hasta la intersección de Turín. Este corredor en ambos sentidos no estará sometido a la medida de pico y placa.</li>
           <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la intersección de la Villa Olímpica. Este corredor en ambos sentidos estará somentido a la medida de pico y placa.</li>
           <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la calle 50, se continúa por la calle 50 hasta la glorieta de la avenida de Las Américas. Este corredor en ambos sentidos estará sometido a la medida de pico y placa.</li>
           <li>De la intersección de la glorieta de la calle 50, se continúa por la avenida de las Américas hasta la intersección de la calle 17 con el Terminal de Transporte. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
           <li>De la intersección de la calle 17 con Terminal de Transporte hacía el norte hasta la intersección de la calle 17 con carrera 13. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
-          <li>De la intersección de la calle 17 con carrera 13 se continúa por la avenida del Ferrocarril hasta el viaducto y la carrera 6a con dirección a la avenida del Rio - puente Mosquera. Este corredor se excluye de la medida de pico y placa.</li>
+          <li>De la intersección de la calle 17 con carrera 13 se continúa por la avenida del Ferrocarril hasta el viaducto y la carrera 6a con dirección a la avenida del Río - puente Mosquera. Este corredor se excluye de la medida de pico y placa.</li>
         </ul>`
       },
       name: "Motos",
