3934373ef236cfd6e992efc489d0c9d7fb30e1fa
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2018-12-21 05:53:31 -0500

    chore(bogota): add particulares exceptions during christmas

--- src/lib/bogota.js
+++ src/lib/bogota.js
@@ -346,6 +346,12 @@ export default {
         na: [0, 6],
         pyp(date) {
           return pypFuncs.pyp(date, this.na, true, () => {
+            if (
+              pypFuncs.formatDate(date) >= "2018-12-26T00:00:00-05:00" &&
+              pypFuncs.formatDate(date) <= "2019-01-04T00:00:00-05:00"
+            ) {
+              return "NA";
+            }
             const pyp = ["0-2-4-6-8", "1-3-5-7-9"];
             return pyp[pypFuncs.getDate(date) % 2];
           });
