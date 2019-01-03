2f00aa0c290eff9893d3e52d2110553d4c9420a9
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2019-01-02 20:20:34 -0500

    chore(tunja): update new info

--- src/lib/tunja.test.js
+++ src/lib/tunja.test.js
@@ -16,6 +16,18 @@ const dateCases = [
       particulares: "9-0",
       taxis: "7-8"
     }
+  },
+  {
+    date: "2019-01-11", // viernes 11 de enero de 2019
+    pypCases: {
+      particulares: "3-4"
+    }
+  },
+  {
+    date: "2019-01-15", // martes 15 de enero de 2019
+    pypCases: {
+      particulares: "5-6"
+    }
   }
 ];
 testCases(city, dateCases);
