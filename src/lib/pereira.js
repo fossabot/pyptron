1e729ae84da7b2605298d002972b1ec2ba0ca5a4
Mauricio Robayo <rfmajo@gmail.com>
2019-02-07 14:08:46 -0500

    chore(pereira): update data with new decree

--- src/lib/pereira.js
+++ src/lib/pereira.js
@@ -10,10 +10,17 @@ module.exports = {
           vehicleClasses: ["Vehículos automotores de servicio particular"],
           decrees: [
             {
-              name: "Decreto 193 del 6 de marzo de 2015",
+              name: "Decreto 872 del 22 de noviembre de 2018",
               url: pypFuncs.buildAssetPath(
                 "pereira",
-                "decreto-193-del-6-de-marzo-de-2015.pdf"
+                "decreto-872-del-22-de-noviembre-de-2018.pdf"
+              )
+            },
+            {
+              name: "Decreto 241 del 26 de marzo de 2016",
+              url: pypFuncs.buildAssetPath(
+                "pereira",
+                "decreto-241-del-26-de-marzo-de-2016.pdf"
               )
             },
             {
@@ -24,10 +31,10 @@ module.exports = {
               )
             },
             {
-              name: "Decreto 241 del 26 de marzo de 2016",
+              name: "Decreto 193 del 6 de marzo de 2015",
               url: pypFuncs.buildAssetPath(
                 "pereira",
-                "decreto-241-del-26-de-marzo-de-2016.pdf"
+                "decreto-193-del-6-de-marzo-de-2015.pdf"
               )
             }
           ],
@@ -44,12 +51,12 @@ module.exports = {
           scheme: "Último dígito del número de la placa según el esquema",
           observations: `Se establece como restricción del pico y placa, para los vehículos particulares y motos, el área al interior del perímeto delimitado por las siguientes vías e interseccones:
         <ul>
-          <li>Partiendo del puente Mosquera se continúa por la avenida del Río hasta la intersección de Turín. Este corredor en ambos sentidos no estará sometido a la medida de pico y placa.</li>
-          <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la intersección de la Villa Olímpica. Este corredor en ambos sentidos estará somentido a la medida de pico y placa.</li>
-          <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la calle 50, se continúa por la calle 50 hasta la glorieta de la avenida de Las Américas. Este corredor en ambos sentidos estará sometido a la medida de pico y placa.</li>
-          <li>De la intersección de la glorieta de la calle 50, se continúa por la avenida de las Américas hasta la intersección de la calle 17 con el Terminal de Transporte. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
-          <li>De la intersección de la calle 17 con Terminal de Transporte hacía el norte hasta la intersección de la calle 17 con carrera 13. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
-          <li>De la intersección de la calle 17 con carrera 13 se continúa por la avenida del Ferrocarril hasta el viaducto y la carrera 6a con dirección a la avenida del Río - puente Mosquera. Este corredor se excluye de la medida de pico y placa.</li>
+          <li>Partiendo del puente Mosquera (punto 1) se continúa por la Avenida del Río hasta la intersección Turín (punto 2). Este corredor en ambos sentidos no estará sometido a la mediada de pico y placa.</li>
+          <li>De la intersección Turín (punto 2) se continúa por la avenida 30 de agosto hasta la intersección Belmonte (punto 3). Este corredor, en ambos sentidos no estará sometido a la medida de pico y placa.</li>
+          <li>De la intersección Belmonte (punto 3) se continúa por la avenida de las Américas hasta l aintersección de la calle 17 con el Terminal de Transportes (punto 4) este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
+          <li>De la intersección de la calle 17 con el Terminal de Transporte (punto 4) hacia el norte, hasta la intersección de la calle 17 con carrera 13 (punto 5). Este corredor se excluye de la medida de pico y placa en ambos sentidos, con los lazos de unión de la calle 17 con la Avenida Belalcázar.</li>
+          <li>De la intersección de la call3 17 con carrera 13 (punto 5) se continúa por la avenida del Ferrocarril hasta el Viaducto y la carrera 6 con dirección a la Avenida del Rio-Puente Mosquera (Punto 1). Este corredor se excluye de la medida del pico y placa, con los lazos de unión de la Glorieta de Bavaria con la Avenida del Ferrocarril.</li>
+          <li>También se excluye de la restrcción del pico y placa el Viaducto César Gaviria Trujillo, y el tramo de la Avenida 30 de agosto, entre la intersección de Turín (punto 2) y la intersección de las carreras 13 y 14 con calle 17 (punto 5).</li>
         </ul>`,
           exceptions: `<ul>
         <li>Carrozas fúnebres y su cortejo.</li>
@@ -71,7 +78,7 @@ module.exports = {
         `,
           map: pypFuncs.buildAssetPath(
             "pereira",
-            "mapa-pico-y-placa-pereira-actualizado.jpg"
+            "mapa-pico-y-placa-pereira-nuevo.jpg"
           )
         },
         name: "Particulares",
@@ -97,10 +104,17 @@ module.exports = {
           vehicleClasses: ["Motocicletas"],
           decrees: [
             {
-              name: "Decreto 193 del 6 de marzo de 2015",
+              name: "Decreto 872 del 22 de noviembre de 2018",
               url: pypFuncs.buildAssetPath(
                 "pereira",
-                "decreto-193-del-6-de-marzo-de-2015.pdf"
+                "decreto-872-del-22-de-noviembre-de-2018.pdf"
+              )
+            },
+            {
+              name: "Decreto 241 del 26 de marzo de 2016",
+              url: pypFuncs.buildAssetPath(
+                "pereira",
+                "decreto-241-del-26-de-marzo-de-2016.pdf"
               )
             },
             {
@@ -111,10 +125,10 @@ module.exports = {
               )
             },
             {
-              name: "Decreto 241 del 26 de marzo de 2016",
+              name: "Decreto 193 del 6 de marzo de 2015",
               url: pypFuncs.buildAssetPath(
                 "pereira",
-                "decreto-241-del-26-de-marzo-de-2016.pdf"
+                "decreto-193-del-6-de-marzo-de-2015.pdf"
               )
             }
           ],
@@ -130,13 +144,13 @@ module.exports = {
           ],
           scheme: "Primer dígito del número de la placa según el esquema",
           observations: `Se establece como restricción del pico y placa, para los vehículos particulares y motos, el área al interior del perímeto delimitado por las siguientes vías e interseccones:
-        <ul>
-          <li>Partiendo del puente Mosquera se continúa por la avenida del Río hasta la intersección de Turín. Este corredor en ambos sentidos no estará sometido a la medida de pico y placa.</li>
-          <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la intersección de la Villa Olímpica. Este corredor en ambos sentidos estará somentido a la medida de pico y placa.</li>
-          <li>De la intersección de Turín se continúa por la avenida 30 de Agosto hasta la calle 50, se continúa por la calle 50 hasta la glorieta de la avenida de Las Américas. Este corredor en ambos sentidos estará sometido a la medida de pico y placa.</li>
-          <li>De la intersección de la glorieta de la calle 50, se continúa por la avenida de las Américas hasta la intersección de la calle 17 con el Terminal de Transporte. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
-          <li>De la intersección de la calle 17 con Terminal de Transporte hacía el norte hasta la intersección de la calle 17 con carrera 13. Este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
-          <li>De la intersección de la calle 17 con carrera 13 se continúa por la avenida del Ferrocarril hasta el viaducto y la carrera 6a con dirección a la avenida del Río - puente Mosquera. Este corredor se excluye de la medida de pico y placa.</li>
+          <ul>
+          <li>Partiendo del puente Mosquera (punto 1) se continúa por la Avenida del Río hasta la intersección Turín (punto 2). Este corredor en ambos sentidos no estará sometido a la mediada de pico y placa.</li>
+          <li>De la intersección Turín (punto 2) se continúa por la avenida 30 de agosto hasta la intersección Belmonte (punto 3). Este corredor, en ambos sentidos no estará sometido a la medida de pico y placa.</li>
+          <li>De la intersección Belmonte (punto 3) se continúa por la avenida de las Américas hasta l aintersección de la calle 17 con el Terminal de Transportes (punto 4) este corredor se excluye de la medida de pico y placa en ambos sentidos.</li>
+          <li>De la intersección de la calle 17 con el Terminal de Transporte (punto 4) hacia el norte, hasta la intersección de la calle 17 con carrera 13 (punto 5). Este corredor se excluye de la medida de pico y placa en ambos sentidos, con los lazos de unión de la calle 17 con la Avenida Belalcázar.</li>
+          <li>De la intersección de la call3 17 con carrera 13 (punto 5) se continúa por la avenida del Ferrocarril hasta el Viaducto y la carrera 6 con dirección a la Avenida del Rio-Puente Mosquera (Punto 1). Este corredor se excluye de la medida del pico y placa, con los lazos de unión de la Glorieta de Bavaria con la Avenida del Ferrocarril.</li>
+          <li>También se excluye de la restrcción del pico y placa el Viaducto César Gaviria Trujillo, y el tramo de la Avenida 30 de agosto, entre la intersección de Turín (punto 2) y la intersección de las carreras 13 y 14 con calle 17 (punto 5).</li>
         </ul>`,
           exceptions: `<ul>
           <li>Carrozas fúnebres y su cortejo.</li>
@@ -157,7 +171,7 @@ module.exports = {
         </ul>`,
           map: pypFuncs.buildAssetPath(
             "pereira",
-            "mapa-pico-y-placa-pereira-actualizado.jpg"
+            "mapa-pico-y-placa-pereira-nuevo.jpg"
           )
         },
         name: "Motos",
