11762b92d23ac65b8039496aa41e9dfe516531ea
Mauricio Robayo <rfmajo@gmail.com>
2019-02-16 10:12:58 -0500

    chore: add bogota/motos excptions

--- src/lib/bogota.js
+++ src/lib/bogota.js
@@ -429,7 +429,25 @@ module.exports = {
             <ul>`,
           observations:
             'No aplicará desde el día hábil siguiente al veinticinco (25) de diciembre de cada año, hasta el viernes hábil antes del descanso ordenado por la Ley 51 de 1983 para la festividad correspondiente al seis (6) de enero del año siguiente.',
-          exceptions: '',
+          exceptions: `<ul>
+          <li>Vehículos de transporte público.</li>
+          <li>Vehículos y motocicletas conducidos por personas en condición de discapacidad o para su transporte.</li>
+          <li>Vehículos y motocicletas de emergencia.</li>
+          <li>Vehículos de transporte escolar de propiedad de instituciones educativas y únicamente cuando sean empleados para el transporte de sus estudiantes. Estos deberán operar y estar plenamente identificados de conformidad con las normas que regulan el transporte escolar.</li>
+          <li>Vehículos de transporte con capacidad para movilizar más de diez (10) pasajeros.</li>
+          <li>Vehículos y motocicletas destinados a operativos de las empresas de servicios públicos domiciliarios.</li>
+          <li>Vehículos y motocicletas destinados al control del tráfico y las grúas que prestan el servicio a la Secretaría Distrital de Movilidad.</li>
+          <li>Caravana Presidencial.</li>
+          <li>Vehículos y motocicletas Militares, de Policía Nacional y de Organismos de Seguridad del Estado.</li>
+          <li>Vehículos y motocicletas asignados al Cuerpo Diplomático.</li>
+          <li>Vehículos con blindaje de nivel tres (3) o superior.</li>
+          <li>Vehículos y motocicletas propulsados exclusivamente por motores eléctricos.</li>
+          <li>Vehículos y motocicletas escolta que estén al servicio de actividades inherentes a la protección de personas debidamente autorizados por la Superintendencia de Vigilancia y Seguridad Privada o que hagan parte de esquemas de seguridad autorizados por los organismos del Estado, y sólo durante la prestación del servicio.</li>
+          <li>Carrozas fúnebres.</li>
+          <li>Motocicletas vinculadas a empresas que prestan el servicio de mensajería debidamente identificadas, o con logos y/o distintivos pintados o adheridos al vehículo y/o plena identificación del conductor del vehículo.</li>
+          <li>Motocicletas vinculadas a establecimientos de comercio que ofrezcan el servicio de domicilio y sean utilizadas exclusivamente para dicha labor y cuenten con identificación, consistente en los logos y/o distintivos pintados o adheridos al vehículo y/o plena identificación del conductor del vehículo.</li>
+          <li>Vehículos y motocicletas vinculados a escuelas de enseñanza automovilística que cumplan con las condiciones establecidas en la normatividad vigente. Vehículos y motocicletas destinados para el control operacional de Transmilenio.</li>
+          <li>Vehículos y motocicletas destinadas al control de emisiones y vertimientos. Automotores y motocicletas utilizados por la Secretaria Distrital de Ambiente o por el Ministerio de Ambiente y Desarrollo Sostenible o quien haga sus veces, para la revisión, atención y prevención de emisiones y vertimientos contaminantes, siempre y cuando cuenten con plena y pública identificación, consistente en los logos pintados o adheridos en el vehículo y/o plena y pública identificación del conductor del vehículo.</li></ul>`,
         },
         name: 'Motos',
         na: [],
