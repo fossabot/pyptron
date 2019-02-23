dadf0a9a9bf394541c228dca80ce19c8b02220ae
Mauricio Robayo <rfmajo@gmail.com>
2019-02-23 09:09:26 -0500

    chore(armenia): add new decree and info

--- src/lib/armenia/motos/info/scheme.js
+++ src/lib/armenia/motos/info/scheme.js
@@ -1,1 +1,2 @@
-module.exports = `Primer dígito del número de la placa según el esquema`
+module.exports =
+  'Restringir la circulación de vehículos automotores particulares tipo motocicleta, mototriciclos, cuatrimotos, motocarros, durante los días hábiles, en el cuadrante comprendido entre la calle 25 a la calle 11 y entre la carrera 22 y la carrera 13, inclusive.'
--- src/lib/armenia/particulares/info/observations.js
+++ src/lib/armenia/particulares/info/observations.js
@@ -1,1 +1,2 @@
-module.exports = ''
+module.exports =
+  'Restringir la circulación de vehículos automotores particulares, durante los días hábiles en el cuadrante comprendido entre la calle 25 a la calle 11 y entre la carrera 22 y la carrera 13, inclusive.'
