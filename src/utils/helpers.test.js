c3b56efb8ebbaef3f58cb9f8654f2693687782f5
Mauricio Robayo <rfmajo@gmail.com>
2019-02-25 07:24:23 -0500

    test: replace snapshots

--- src/utils/helpers.test.js
+++ src/utils/helpers.test.js
@@ -22,9 +22,8 @@ describe('Generate pyp object maps of global info with the slug and the key', ()
     const categoriesMap = getCategories('bogota')
     const categories = Object.keys(categoriesMap)
     expect(typeof categoriesMap).toBe('object')
-    expect(categories.length).toBe(5)
+    expect(categories.length).toBeGreaterThan(0)
     categories.forEach(category => {
-      expect(typeof categoriesMap[category]).toBe('object')
       expect(Object.keys(categoriesMap[category]).length).toBe(2)
       expect(typeof categoriesMap[category].name).toBe('string')
       expect(categoriesMap[category].name.length).toBeGreaterThan(0)
@@ -56,8 +55,43 @@ describe('Generate pyp data objects', () => {
     const result = getPypData('bogota', date, days)
     expect(result.data.length).toBe(3)
     expect(result.name).toBe('Bogotá')
-    console.log(result)
-    expect(result).toMatchSnapshot()
+    expect(result.path).toBe('bogota')
+    const categories = Object.keys(result.info)
+    expect(categories.length).toBeGreaterThan(0)
+    const categoryKeys = [
+      'days',
+      'decrees',
+      'exceptions',
+      'hours',
+      'maps',
+      'observations',
+      'scheme',
+      'vehicleClasses',
+      'name',
+      'path',
+    ]
+    categories.forEach(category => {
+      expect(Object.keys(result.info[category])).toEqual(categoryKeys)
+    })
+    const { data } = result
+    expect(data.length).toBe(days)
+    data.forEach(elem => {
+      expect(elem.date).toEqual(
+        expect.stringMatching(
+          /20\d{2}-[0-1]\d{1}-[0-3]\d{1}T\d{2}:\d{2}:\d{2}.\d{3}Z/
+        )
+      )
+      elem.categories.forEach(category => {
+        expect(category).toEqual(
+          expect.objectContaining({
+            name: expect.any(String),
+            path: expect.stringMatching(/[a-z]+\/[a-z]+/),
+            key: expect.stringMatching(/[a-z]+/),
+            pyp: expect.stringMatching(/[\d-]+/),
+          })
+        )
+      })
+    })
   })
   it('should return data for two categories', () => {
     const date = '2018-03-06T00:00:02-05:00'
@@ -73,10 +107,35 @@ describe('Generate pyp data objects', () => {
   })
   it('should return pyp meta-info for all categories', () => {
     const date = '2018-03-06T00:00:02-05:00'
-    expect(getPypInfo('bogota', date)).toMatchSnapshot()
+    const result = getPypInfo('bogota', date)
+    const categoryKeys = [
+      'days',
+      'decrees',
+      'exceptions',
+      'hours',
+      'maps',
+      'observations',
+      'scheme',
+      'vehicleClasses',
+      'name',
+      'path',
+    ]
+    Object.keys(result).forEach(category => {
+      expect(Object.keys(result[category])).toEqual(categoryKeys)
+    })
   })
   it('should return pyp data for one date', () => {
     const date = '2018-03-06T00:00:02-05:00'
-    expect(getPyp('bogota', date)).toMatchSnapshot()
+    const result = getPyp('bogota', date)
+    expect(result).toEqual(
+      expect.arrayContaining([
+        {
+          name: expect.any(String),
+          path: expect.stringMatching(/[a-z]+\/[a-z]+/),
+          key: expect.stringMatching(/[a-z]+/),
+          pyp: expect.stringMatching(/[\d-]+/),
+        },
+      ])
+    )
   })
 })
