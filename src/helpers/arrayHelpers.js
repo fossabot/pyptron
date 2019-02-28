196f25d607efb5b25bd790fac68a498e962d49a2
Mauricio Robayo <rfmajo@gmail.com>
2019-02-28 05:52:36 -0500

    chore(helpers): clean comments

--- src/helpers/arrayHelpers.js
+++ src/helpers/arrayHelpers.js
@@ -10,10 +10,7 @@ module.exports = {
 }
 
 /**
- * Calcula el índice para cuando el valor del índice es mayor al largo del array o en caso de que se
- * tenga un valor negativo para el índice. Si el valor es negativo el valor correspondiente al
- * indice se calcula de atrás hacia adelante. Es decir, -1 equivale al último elemento del array y
- * -array.length equivale al primer elemento del array.
+ * Calcula el índice para cuando el valor del índice es mayor al largo del array o en caso de que se tenga un valor negativo para el índice. Si el valor es negativo el valor correspondiente al indice se calcula de atrás hacia adelante. Es decir, -1 equivale al último elemento del array y -array.length equivale al primer elemento del array.
  * @param {int} index Valor para el cual se desea calcular el índice correspondiente en el array.
  * @param {int} arrLen Largo del array.
  * @returns {int} El índice - en caso de ser negativo corresponde al indice de atrás hacia adelante.
@@ -23,8 +20,7 @@ function getIndex(index, arrLen) {
 }
 
 /**
- * Rota los elementos de un array desplazando sus elementos según la cantidad indicada en el offset
- * de izquierda a derecha. Por ejemplo:
+ * Rota los elementos de un array desplazando sus elementos según la cantidad indicada en el offset de izquierda a derecha. Por ejemplo:
  *   arrRotate([1, 2, 3], 1) // [3, 1, 2]
  *   arrRotate([1, 2, 3], -1) // [2, 3, 1]
  * @param {array} arr El array cuyos elementos se desean rotar.
@@ -55,7 +51,6 @@ function arrRotate(arr, offset) {
  * @returns {string} Valor de pypNums correspondiente a date tras la rotación de los elementos.
  */
 function rotateByDay(date, startDate, startNums, pypNums, skip = [0]) {
-  // eslint-disable-next-line no-unmodified-loop-condition
   let daysLapse = daysDiff(startDate, date, { skip })
   daysLapse += pypNums.indexOf(startNums) - 1
   return pypNums[daysLapse % pypNums.length]
@@ -87,7 +82,6 @@ function rotateBy(
   const diff = datesDiff(startDateObject, dateObject, period)
   const lapse = Math.ceil((reverse ? -diff : diff) / interval)
   const pypArray = arrRotate(pypNums, lapse - pypOffset)
-  // La diferencia entre mDate.day() y mStartDate.day() puede ser negativa por lo tanto calculamos el índice en caso de que sea negativo, en cuyo caso el cálculo se haría de atrás hacia adelante, siendo -1 el último elemento del array.
   const index = getIndex(
     dateObject.getDay() - startDateObject.getDay(),
     pypArray.length
