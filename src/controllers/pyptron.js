b0092044f3570495d3fa662815dfd107c68d6d7a
Mauricio Robayo <rfmajo@gmail.com>
2019-03-01 18:32:33 -0500

    style(pyptron): remove jsdocs comments

--- src/controllers/pyptron.js
+++ src/controllers/pyptron.js
@@ -69,20 +69,6 @@ module.exports = {
   getPypInfo,
 }
 
-/**
- * Obtiene las categorías correspondientes a una ciudad dada en donde la llave es el slug ordenado
- * alfabéticamente y el valor es otro objecto que contiene la llave interna para acceder a la
- * categoría y el nombre humano para la categoría. Ejemplo:
- *   const manizalesCategories = getCategories('manizales')
- *   { taxis: {
- *       key: 'taxis',
- *       name: 'Taxis' },
- *     'transporte-publico-colectivo': {
- *       key: 'tpc',
- *       name: 'Transporte Público Colectivo' } }
- * @param {string} city La ciudad para la cual se desean consultar las categorías.
- * @returns {Object} Las categorías correspondientes a la ciudad con la llave y el nombre humano.
- */
 function getCategories(city) {
   const categoriesMap = {}
   const cityObj = cities[city]
@@ -95,39 +81,6 @@ function getCategories(city) {
   return categoriesMap
 }
 
-/**
- * Devuelve un objecto con la información correspondiente a las ciudades almacenadas en donde la
- * llave es el slug ordenado alfabéticamente y el valor es un objeto con tres valores que son la llave interna para
- * acceder a la categoría, el nombre humano para la categoría y la lista de categorías disponibles.
- * Ejemplo:
- * const cities = getCities();
- * { armenia:
- *   { name: 'Armenia',
- *     key: 'armenia',
- *     categories: { motos: [Object], particulares: [Object], taxis: [Object] } },
- *  barranquilla:
- *   { name: 'Barranquilla',
- *     key: 'barranquilla',
- *     categories: { taxis: [Object] } },
- *  bogota:
- *   { name: 'Bogotá',
- *     key: 'bogota',
- *     categories:
- *       { ambiental: [Object],
- *         'servicio-de-transporte-especial': [Object],
- *         particulares: [Object],
- *         taxis: [Object],
- *         'transporte-publico-colectivo': [Object] } },
- *  bucaramanga:
- *   { name: 'Bucaramanga',
- *     key: 'bucaramanga',
- *     categories:
- *       { motos: [Object],
- *         particulares: [Object],
- *         taxis: [Object] } },
- *  ...
- * @returns {Object} Las ciudades disponibles con sus correspondientes categorías.
- */
 function getCities() {
   return Object.keys(cities)
     .sort()
@@ -143,15 +96,7 @@ function getCities() {
       return result
     }, {})
 }
-/**
- * Devuelve un array con las categorías y los valores correspondientes al pico y placa para la
- * ciduad y la fecha solicitada. Si no se especifica la lista de categorías que se desea por defecto
- * se devuelven todas las categorías disponibles para la ciudad solicitada.
- * @param {string} city Ciudad para la cual se desea obtener la información
- * @param {string} date Fecha para la cual se desea obtener la información.
- * @param {Array} categories Opcional lista de categorías que se desean consultar.
- * @returns {Array} El valor del pyp para la ciudad y categorías solicitadas.
- */
+
 function getPyp(city, date, categories = []) {
   const pypCity = cities[city]
   const cats = categories.length
@@ -173,15 +118,6 @@ function getPyp(city, date, categories = []) {
   }, [])
 }
 
-/**
- * Devuelve un objeto conla metainformación para la ciudad solicitada en donde las llaves son el
- * slug de las categorías correspondientes a la ciudad dada ordenada alfabéticamente. Si no se
- * especifican categorías por defecto se devuelven todas las categorías disponibles.
- * @param {string} city Ciudad para la cual se desea consultar la meta-información.
- * @param {string} date Fecha para la cual se desea obtener la información.
- * @param {Array} categories Opcional lista de categorías que se desean consultar.
- * @returns {Object} La meta-información para la ciudad y categorías solicitadas.
- */
 function getPypInfo(city, date, categories = []) {
   const pypCity = cities[city]
   const cats = categories.length
@@ -203,19 +139,6 @@ function getPypInfo(city, date, categories = []) {
   }, {})
 }
 
-/**
- * Devuelve la metainformación y la información correspondiente a la ciudad solicitada para la
- * fecha solicitada. Si no se especifican los días se desean sólo se devuelve la información para un
- * día correspondiente a la fecha solicita, de lo contrario, se devuelve la información de la
- * cantidad de días solicitados a partir de la fecha dada. Si no se especifican las categorías que
- * se desean consultar por defecto se devuelven todas las categorías disponibles para la ciudad
- * solicitada.
- * @param {string} city La ciudad para la cual se desea consultar la información.
- * @param {string} date La fecha para la cual se desea solicitar la información.
- * @param {Number} days Cantidad de días que se deasean consultar a partir de la fecha dada.
- * @param {Array} categories Lista de categorías que se desean consultar.
- * @returns {Object} Toda la información correspondiente a la ciudad, categorías y días solicitados.
- */
 function getPypData(city, date, days = 1, categories = []) {
   // Si el argumento `date` solo tiene diez caracteres quiere decir que no se ha
   // indicado la zona horaria por lo que asumimos la zona horario de Colombia.
