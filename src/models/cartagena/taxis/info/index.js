d29a4f8d665b7d478b7b33730de04c9e73ca1baa
Mauricio Robayo <rfmajo@gmail.com>
2019-03-03 06:50:56 -0500

    chore(cartagena): add decree url

--- src/models/cartagena/taxis/info/index.js
+++ src/models/cartagena/taxis/info/index.js
@@ -7,7 +7,11 @@ module.exports = {
     'Lunes a viernes (No se aplicará en los días festivos establecidos por la Ley)',
   ],
   decrees: [
-    { name: 'Decreto 1483 de diciembre de 2018', url: '' },
+    {
+      name: 'Decreto 1483 de diciembre de 2018',
+      url:
+        'http://www.transitocartagena.gov.co/links/Decretos/Decretos_2018/Decreto_1483.PDF',
+    },
     { name: 'Decreto 1659 de diciembre de 2017', url: 'cdn:pdf' },
   ],
   exceptions,
