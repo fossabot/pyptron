3a06b36ce5afce8327464b5d29a54ef5754913c2
Mauricio Robayo <rfmajo@gmail.com>
2019-03-06 05:49:53 -0500

    chore(medellin): add aditional hours info

--- src/models/medellin/motos/info/index.js
+++ src/models/medellin/motos/info/index.js
@@ -15,6 +15,11 @@ module.exports = {
   hours: [
     // { comment: '', hours: [['7:00', '8:30'], ['17:30', '19:00']], days: [] },
     { comment: '', hours: [['5:00', '10:00'], ['16:00', '20:00']], days: [] },
+    {
+      comment: 'Modelos anteriores o iguales a 1996',
+      hours: [['5:00', '21:00']],
+      days: [],
+    },
   ],
   maps: [],
   observations,
