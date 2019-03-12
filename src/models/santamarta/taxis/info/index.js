f8dadd46863033a5086be1bc626083085512711e
Mauricio Robayo <rfmajo@gmail.com>
2019-03-12 11:17:18 -0500

    chore(santa-marta): reorder decrees

--- src/models/santamarta/taxis/info/index.js
+++ src/models/santamarta/taxis/info/index.js
@@ -5,8 +5,8 @@ const observations = require('./observations.md')
 module.exports = {
   days: ['Lunes a sábado'],
   decrees: [
-    { name: 'decreto 068 de 2018', url: 'cdn:pdf' },
     { name: 'Decreto 043 de 2019', url: 'cdn:pdf' },
+    { name: 'decreto 068 de 2018', url: 'cdn:pdf' },
   ],
   exceptions,
   hours: [{ comment: '', hours: [['7:00', '23:59']], days: [] }],
