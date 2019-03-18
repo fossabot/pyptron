e0110e4f5ba4409931aa5c349eff2820512f8277
Mauricio Robayo <rfmajo@gmail.com>
2019-03-18 05:42:29 -0500

    chore(medellin): add messages

--- src/models/medellin/index.js
+++ src/models/medellin/index.js
@@ -5,6 +5,13 @@ const taxis = require('./taxis')
 
 module.exports = {
   name: 'Medellín',
+  messages: [
+    {
+      message:
+        'Ampliación de horarios del 18 al 20 de marzo por estado de alerta por calidad del aire.',
+      url: 'https://twitter.com/Areametropol/status/1107327209252048897',
+    },
+  ],
   categories: {
     carga,
     motos,
