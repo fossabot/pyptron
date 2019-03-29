b5307bc208a043beb09fb566a1bf4709643f5065
Mauricio Robayo <rfmajo@gmail.com>
2019-03-29 06:54:57 -0500

    chore(medellin): add messages

--- src/models/medellin/index.js
+++ src/models/medellin/index.js
@@ -5,7 +5,13 @@ const taxis = require('./taxis')
 
 module.exports = {
   name: 'Medellín',
-  messages: [],
+  messages: [
+    {
+      message:
+        'Estado de prevención por calidad del aire en los 10 municipios del Valle de Aburrá para el viernes 29 y el sábado 30 de marzo.',
+      url: 'https://twitter.com/Areametropol/status/1111437594447896577',
+    },
+  ],
   categories: {
     carga,
     motos,
