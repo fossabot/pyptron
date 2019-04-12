05cfe92fac3f8cf10fb8634643b51bf9ab7899b1
Mauricio Robayo <rfmajo@gmail.com>
2019-04-11 20:51:56 -0500

    chore(armenia): update semana santa

---
+++ src/models/armenia/particulares/190422/index.js
@@ -0,0 +1,7 @@
+const PypData = require('../../../../classes/pyp-data')
+const pyp190407 = require('../190407')
+
+module.exports = new PypData({
+  ...pyp190407,
+  startDate: '2019-04-22',
+})
--- src/models/armenia/particulares/index.js
+++ src/models/armenia/particulares/index.js
@@ -1,7 +1,10 @@
 const Category = require('../../../classes/category')
 const pyp190407 = require('./190407')
+const pyp190415 = require('./190415')
+const pyp190422 = require('./190422')
 
 module.exports = new Category({
   key: 'particulares',
-  pypData: [pyp190407],
+  messages: [],
+  pypData: [pyp190407, pyp190415, pyp190422],
 })
