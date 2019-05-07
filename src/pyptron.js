3bea8095368f16709096466f6d1b510d1366ad01
Mauricio Robayo <rfmajo@gmail.com>
2019-05-07 15:43:47 -0500

    chore: remove createresponse from main

--- src/pyptron.js
+++ src/pyptron.js
@@ -1,5 +1,4 @@
 const { getCitiesMap, getCityData } = require('./controllers')
-const { createResponse } = require('./helpers/global-helpers')
 
 exports.pyptron = async event => {
   const { city, category } = event.pathParameters || {}
@@ -12,8 +11,6 @@ exports.pyptron = async event => {
     case '/{city}/{category}':
       return getCityData(city, { category, ...options })
     default:
-      return createResponse(500, {
-        error: 'Only God knows what just happened.',
-      })
+      return 'Only God knows what just happened.'
   }
 }
