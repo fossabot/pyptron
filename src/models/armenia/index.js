476cc32a3dbaa811859c1849504d459f012ff336
Mauricio Robayo <rfmajo@gmail.com>
2019-06-22 13:10:57 -0500

    chore(armenia): remove outdated message

--- src/models/armenia/index.js
+++ src/models/armenia/index.js
@@ -5,13 +5,7 @@ const taxis = require('./taxis')
 
 module.exports = new City({
   name: 'Armenia',
-  messages: [
-    {
-      message:
-        'Se levanta la medida de Pico y Placa en toda la ciudad para toda clase de vehículos particulares a excepción de las motocicletas y similares.',
-      url: 'https://twitter.com/AlcaldiaArmenia/status/1116466337172402176',
-    },
-  ],
+  messages: [],
   categories: {
     motos,
     particulares,
