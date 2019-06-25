e510eae32bcacda1199e4a137c9c38ff9bc6181f
Mauricio Robayo <rfmajo@gmail.com>
2019-06-24 21:35:10 -0500

    feat(villavicencio): include temporary meassure

--- src/models/villavicencio/index.js
+++ src/models/villavicencio/index.js
@@ -6,6 +6,13 @@ const taxis = require('./taxis')
 
 module.exports = new City({
   name: 'Villavicencio',
+  messages: [
+    {
+      message:
+        'Entre el 26 de junio y el 3 de julio se levanta temporalmente la medida del pico y placa en el centro de la ciudad.',
+      url: 'https://twitter.com/WilmarBarbosa/status/1142121584955678726',
+    },
+  ],
   categories: {
     carga,
     motos,
--- src/models/villavicencio/particulares/index.js
+++ src/models/villavicencio/particulares/index.js
@@ -1,7 +1,9 @@
 const Category = require('../../../classes/category')
 const pyp190407 = require('./190407')
+const pyp190626 = require('./190626')
+const pyp190704 = require('./190704')
 
 module.exports = new Category({
   ...Category.metainfo.particulares,
-  pypData: [pyp190407],
+  pypData: [pyp190407, pyp190626, pyp190704],
 })
