ad8895a4f54cdb8e2f6af4b89b843d7a1fe0cdb9
Mauricio Robayo <rfmajo@gmail.com>
2019-09-27 15:34:57 -0500

    fix(medellin): restore pyp after temp measure

--- src/models/medellin/particulares/index.js
+++ src/models/medellin/particulares/index.js
@@ -1,8 +1,9 @@
 const Category = require('../../../classes/category')
 const pyp190407 = require('./190407')
 const pyp190930 = require('./190930')
+const pyp191102 = require('./191102')
 
 module.exports = new Category({
   ...Category.metainfo.particulares,
-  pypData: [pyp190407, pyp190930],
+  pypData: [pyp190407, pyp190930, pyp191102],
 })
