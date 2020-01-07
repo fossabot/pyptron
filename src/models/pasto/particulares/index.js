aef09c9aa6dcc29b41ab5626f048a8629e164f52
Mauricio Robayo <rfmajo@gmail.com>
2020-01-07 11:21:01 -0500

    chore(pasto): add another decree

--- src/models/pasto/particulares/index.js
+++ src/models/pasto/particulares/index.js
@@ -6,6 +6,11 @@ module.exports = new Category({
   pypData: [pyp190407],
   decrees: [
     {
+      name: 'Decreto 0184 del 8 de junio de 2018',
+      url:
+        'https://www.pasto.gov.co/index.php/decretos/decretos-2018?download=12087:dec-0184-08-jun-2018',
+    },
+    {
       name: 'Decreto 0201 del 18 de mayo de 2017',
       url:
         'http://www.pasto.gov.co/index.php/decretos/decretos-2017?download=10061:dec_0201_18_may_2017',
