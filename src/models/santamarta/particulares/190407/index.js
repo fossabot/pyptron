fb99991224dad790159af5bfe792770dfd74bef5
Mauricio Robayo <rfmajo@gmail.com>
2020-01-07 12:01:30 -0500

    style(santamarta): remove useless comments

--- src/models/santamarta/particulares/190407/index.js
+++ src/models/santamarta/particulares/190407/index.js
@@ -15,11 +15,11 @@ module.exports = new PypData({
   skipHolidays: true,
   pypFunction(date) {
     const pyp = [
-      [6, 7], // lunes
-      [8, 9], // martes
-      [0, 1], // miércoles
-      [2, 3], // jueves
-      [4, 5], // viernes
+      [6, 7],
+      [8, 9],
+      [0, 1],
+      [2, 3],
+      [4, 5],
     ]
     return pyp[newISODate(date).getDay() - 1]
   },
