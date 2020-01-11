b79b0c8c2d31c1867a2e49c44f967b0537288cdd
Mauricio Robayo <me@mauriciorobayo.com>
2020-01-11 12:10:26 -0500

    chore(cali): remove message

--- src/models/cali/index.js
+++ src/models/cali/index.js
@@ -5,14 +5,6 @@ const tpc = require('./tpc')
 
 module.exports = new City({
   name: 'Cali',
-  messages: [
-    {
-      message:
-        'En la actualidad, no rige medida de pico y placa en Santiago de Cali',
-      url:
-        'https://www.cali.gov.co/movilidad/publicaciones/151255/en-la-actualidad-no-rige-medida-de-pico-y-placa-en-santiago-de-cali/',
-    },
-  ],
   categories: {
     particulares,
     taxis,
